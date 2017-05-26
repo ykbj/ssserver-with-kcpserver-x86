b90010c67bb748f8d6ef39d06608d00c3eaeae50
phoenix.lv <phoenix.lv@dorry.io>
2017-05-26 09:15:32 +0800

    change the ss kcp config file into /home/sskcpserver folder

--- sskcpserver_start.sh
+++ sskcpserver_start.sh
@@ -1,4 +1,5 @@
 sudo mkdir -p /dorry_data/sskcp_conf
+sudo mkdir -p /dorry_data/kcplog
 sudo cp kcp.json /dorry_data/sskcp_conf/
 sudo cp shadowsocks.json /dorry_data/sskcp_conf/
-docker run -itd --cap-add=NET_ADMIN --network=host --restart=always --privileged -v /dorry_data/sskcp_conf/:/home/ --name=ss-kcp-server dorrychocolate/ssserver-with-kcpserver-x86:test
+docker run -itd --cap-add=NET_ADMIN --network=host --restart=always --privileged -v /dorry_data/sskcp_conf/:/home/sskcpserver -v /dorry_data/kcplog:/home/kcplog --name=ss-kcp-server dorrychocolate/ssserver-with-kcpserver-x86:test
